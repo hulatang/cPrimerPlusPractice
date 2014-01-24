a6a55482b8d681277dd1de9e6e8dc9bd4b764862
hulatang <chenbineric@gmail.com>
2014-01-24 16:41:03 +0800

    platinum.c added

---
+++ platinum.c
@@ -0,0 +1,25 @@
+/* platinum.c, measure your weight in platinum 
+ */
+
+#include <stdio.h>
+int main(void)
+{
+	float weight;
+	float value;
+
+	printf("Are you worth your weight in platinum?\n");
+	printf("Let's check it out.\n");
+	printf("Please Enter Your Weight:\n");
+
+	/* get input from the user */
+	scanf("%f", &weight);
+
+	/*assume platinum is $1700 per ounce */
+	/* 14.5833 converst pounds to ounce , 1 pound = 14.5833 ounce */
+
+	value = 1700.0 * weight * 14.5833;
+	printf("Your weight in platinum is worth $%.2f.\n", value);
+	printf("eat more to maintain your value.\n");
+
+	return 0;
+}
