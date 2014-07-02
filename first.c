f9dd806a9b15ad8f4bd6cd1c4d95f5cc18e7e312
chenbinarctic@163.com <chenbinarctic@163.com>
2014-07-02 22:52:13 +0800

    fixed

--- first.c
+++ first.c
@@ -1,13 +1,7 @@
-//the first test .c file
-<<<<<<< HEAD
-//hello, try again
-=======
-//hello, revised on my mac
->>>>>>> 7df2359dc8b6d0faa93904bafd3d071822944975
-
 #include <stdio.h>
 int main(void)
 {
 	printf("Hello, I'm here");
 	printf("Hello, try push on Ubuntu"); // if this line can be seen, push ok
 }
+hello1
